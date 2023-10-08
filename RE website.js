f0d2ffba4eea851f6914a9f59ba74fd64185d43d
Basant1232 <137476019+Basant1232@users.noreply.github.com>
2023-10-08 22:41:05 +0530

    Add files via upload

---
+++ RE website.js
@@ -0,0 +1,91 @@
+// nav part 2
+
+function a(){
+    document.getElementById("1nav2").src="./images/3rd img down nav p2.avif"
+}
+function b(){
+    document.getElementById("1nav2").src="./images/4th img down nav p2.avif" 
+}
+function c(){
+    document.getElementById("1nav2").src="./images/5th img down nav p2.avif" 
+}
+function d(){
+    document.getElementById("1nav2").src="./images/6th img down nav p2.avif"
+}
+function e(){
+    document.getElementById("1nav2").src="./images/7th img down nav p2.avif" 
+}
+function f(){
+    document.getElementById("1nav2").src="./images/8th img down nav p2.avif" 
+}
+function g(){
+    document.getElementById("1nav2").src="./images/9th img down nav p2.avif"
+}
+function h(){
+    document.getElementById("1nav2").src="./images/10th img down nav p2.avif" 
+}
+function a1(){
+    document.getElementById("1nav2").src="./images/Headersection img.avif "
+}
+
+// nav part 3
+
+function b1(){
+    document.getElementById("2nav2").src="./images/HeaderSection img 2.avif"
+}
+function b2(){
+    document.getElementById("2nav2").src="./images/2nd img down nav p3.avif" 
+}
+function b3(){
+    document.getElementById("2nav2").src="./images/3nd img down nav p3.avif" 
+}
+function b4(){
+    document.getElementById("2nav2").src="./images/4nd img down nav p3.avif"
+}
+function b5(){
+    document.getElementById("2nav2").src="./images/5nd img down nav p3.avif" 
+}
+function b6(){
+    document.getElementById("2nav2").src="./images/6nd img down nav p3.avif"
+}
+
+// nav part 4
+
+function c1(){
+    document.getElementById("3nav2").src="./images/Headersection img 3.avif"
+}
+function c2(){
+    document.getElementById("3nav2").src="./images/2nd img down nav p4.avif"
+}
+function c3(){
+    document.getElementById("3nav2").src="./images/3nd img down nav p4.avif" 
+}
+function c4(){
+    document.getElementById("3nav2").src="./images/4nd img down nav p4.avif"
+}
+function c5(){
+    document.getElementById("3nav2").src="./images/5nd img down nav p4.avif"
+}
+function c6(){
+    document.getElementById("3nav2").src="./images/6nd img down nav p4.avif"
+}
+
+// nav part 5
+
+function d1(){
+    document.getElementById("4nav2").src="./images/Headersection img 4.avif"
+}
+function d2(){
+    document.getElementById("4nav2").src="./images/2nd img down nav p5.avif"
+}
+
+// nav part 6
+function e1(){
+    document.getElementById("5nav2").src="./images/Headersection img 5.avif"
+}
+function e2(){
+    document.getElementById("5nav2").src="./images/2nd img down nav p6.avif"
+}
+function e3(){
+    document.getElementById("5nav2").src="./images/3nd img down nav p6.avif" 
+}
